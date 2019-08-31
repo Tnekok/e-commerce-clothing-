a33da9a1ec1c5577a344f072730c948307399795
Tnekok <jesmgarrido@gmail.com>
2019-08-31 17:07:50 +0200

    updated shop data with new data, added cartdropdown component and cart redux logic

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,8 +7,11 @@ import { auth } from '../../firebase/firebase.utils';
 import './header.styles.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({ currentUser }) => (
+
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
             <Logo className='logo' />
@@ -22,12 +25,15 @@ const Header = ({ currentUser }) => (
                     :
                     <Link className='option' to='/signin'>SIGN IN</Link>
             }
+            <CartIcon />
         </div>
+        { hidden ? null : <CartDropdown /> }
     </div>
 );
 
-const mapsStateToProps = ( state ) => ({
-    currentUser: state.user.currentUser
+const mapsStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
 });
 
 export default connect(mapsStateToProps)(Header);
---
+++ src/redux/cart/cart.reducers.js
@@ -0,0 +1,26 @@
+import CartActionTypes from './cart.types';
+import { addItemToCart } from './cart.utils';
+
+const INITIAL_STATE = {
+    hidden: true,
+    cartItems: []
+};
+
+const cartReducer = ( currentState = INITIAL_STATE, action ) => {
+    switch (action.type) {
+        case CartActionTypes.TOGGLE_CART_HIDDEN:
+            return {
+                ...currentState,
+                hidden: !currentState.hidden
+            };
+        case CartActionTypes.ADD_ITEM:
+            return {
+                ...currentState,
+                cartItems: addItemToCart(currentState.cartItems, action.payload)
+            };
+        default:
+            return currentState;
+    }
+};
+
+export default cartReducer;
