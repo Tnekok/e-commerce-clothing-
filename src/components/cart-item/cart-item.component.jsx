41c06109721f0bd106fdcfaf542e3431f643f1ad
Tnekok <jesmgarrido@gmail.com>
2019-08-31 17:11:27 +0200

    small naming fix for styling the cart item info

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -5,7 +5,7 @@ import './cart-item.styles.scss';
 const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
     <div className='cart-item'>
         <img src={imageUrl} alt ='item' />
-        <div className='otem-container'>
+        <div className='item-details'>
             <span className='name'>{name}</span>
             <span className='price'>{quantity} x ${price}</span>
         </div>
